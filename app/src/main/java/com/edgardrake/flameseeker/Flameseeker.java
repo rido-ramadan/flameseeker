a97a1a49def0541aa48e2f100a7cc0b6f7434727
Rido Ramadan <rido.ramadan@gmail.com>
2017-01-24 10:45:07 +0700

    Flameseeker first commit.

---
+++ app/src/main/java/com/edgardrake/flameseeker/Flameseeker.java
@@ -0,0 +1,15 @@
+package com.edgardrake.flameseeker;
+
+import android.app.Application;
+
+/**
+ * Created by Edgar Drake on 23-Jan-17.
+ */
+
+public class Flameseeker extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/edgardrake/flameseeker/fcm/FlameseekerInstanceIDService.java
@@ -0,0 +1,27 @@
+package com.edgardrake.flameseeker.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Edgar Drake on 23-Jan-17.
+ */
+
+public class FlameseekerInstanceIDService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("FCM ID", "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+}
