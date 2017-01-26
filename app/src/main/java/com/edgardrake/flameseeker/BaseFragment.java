6c3b79325a0566acaf07f9141829f1924116afed
Rido Ramadan <rido.ramadan@gmail.com>
2017-01-26 16:53:31 +0700

    Base fragment.

---
+++ app/src/main/java/com/edgardrake/flameseeker/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.edgardrake.flameseeker;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Edgar Drake on 26-Jan-17.
+ */
+
+public class BaseFragment extends Fragment {
+}
